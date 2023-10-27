22831fa0fc6951c472cc7263e0c0c1875e10e477
maximumry <144888797+maximumry@users.noreply.github.com>
2023-10-27 17:41:34 +0900

    createアクションの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,26 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.preload(:user).order("created_at DESC")
   end
 
   def new
     @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to root_path
+    # if post.valid?
+    #   post.save
+    #   redirect_to root_path
+    # else
+    #   render :new, status: :unprocessable_entity
+    # end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:asset, :text).merge(user_id: current_user.id)
   end
 end
