2e065a489c1a569383a51e5b0f6897dc518bc8a0
maximumry <teapots-thorium.07@icloud.com>
2023-10-28 20:31:45 +0900

    ユーザー情報のedit実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
-  resources :users, only: :show
+  resources :users, only: [:show, :edit, :update]
   resources :posts, only: [:index, :new, :create]
 end
