85a713d9df2eb48bf94d5a3f80739cf33b2fc0ac
maximumry <teapots-thorium.07@icloud.com>
2023-10-30 12:08:58 +0900

    postモデルのテスト実施

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :post do
-    asset {Faker::Number.between(to: 14)}
-    text {Faker::Loren.sentence}
+    asset {Faker::Number.number(digits: 14)}
+    text {Faker::Lorem.sentence}
     association :user
   end
 end
