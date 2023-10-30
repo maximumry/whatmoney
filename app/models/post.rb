78c990fff07989891801520915b9d9af6026193d
maximumry <teapots-thorium.07@icloud.com>
2023-10-30 12:31:51 +0900

    画像かテキストのみの投稿のバリデーション設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :asset, presence: true, numericality: { only_integer: true }
+  validates :asset, presence: true, numericality: { only_integer: true }, unless: :was_attached?
   validates :text, presence: true
+
+  def was_attached?
+    self.image.attached?
+  end
 end
