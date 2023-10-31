a36f084ada90fcd8cf645aad2f6230e8253033aa
maximumry <144888797+maximumry@users.noreply.github.com>
2023-10-31 12:14:18 +0900

    ログインの有無

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: :index
   before_action :user_match, only: [:edit, :destroy]
   def index
     @posts = Post.preload(:user).order("created_at DESC")
