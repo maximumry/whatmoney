a76485d5958b50dfc3fa2c52f6576ab381b21496
maximumry <teapots-thorium.07@icloud.com>
2023-10-31 11:02:19 +0900

    editのビューまで実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,6 +21,9 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
   private
   def post_params
     params.require(:post).permit(:asset, :text, :image).merge(user_id: current_user.id)
