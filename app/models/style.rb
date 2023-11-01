af863f7508bfc7ed2e6f2efc59459666dc286998
maximumry <teapots-thorium.07@icloud.com>
2023-11-01 20:00:05 +0900

    投資のスタイルのアクティブハッシュ

---
+++ app/models/style.rb
@@ -0,0 +1,11 @@
+class Style < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '___' },
+    { id: 2, name: 'スキャルピング' },
+    { id: 3, name: 'デイトレード' },
+    { id: 4, name: 'スイングトレード' },
+    { id: 5, name: '長期投資' },
+    { id: 6, name: 'インデックス投資' },
+    { id: 7, name: 'バリュー株' },
+    { id: 8, name: 'グロース株' }
+  ]
